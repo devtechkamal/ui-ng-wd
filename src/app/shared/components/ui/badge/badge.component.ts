7245dfb0dd798adecd7c8254eef623c45950bf3f
devtechkamal <131899275+devtechkamal@users.noreply.github.com>
2026-01-23 21:48:56 +0530

    Add Badge component
    Add Badge component

---
+++ src/app/shared/components/ui/badge/badge.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wdc-badge',
+  imports: [],
+  templateUrl: './badge.component.html',
+  styleUrl: './badge.component.css',
+})
+export class BadgeComponent {
+
+}
